a26c439ad180797a350157c4f0540941b4cfa4e0
David <leinerdavidgp@gmail.com>
2025-10-23 13:15:22 -0300

    Estado
    - Se eliminó la acción dibujar.
    - Se agregaron comentario al código.
    - Se añadió la acción "/estado" para mostarar el estado del jugador.
    -

--- src/todo/FlujoPrincipal.java
+++ src/todo/FlujoPrincipal.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class FlujoPrincipal {
 
 	public static void main(String[] args) {
+		//Scanner para input de juagador
 		Scanner sc = new Scanner(System.in);
+		
+		//Generar instancia del jugador
 		Jugador jugadorActual = new Jugador("Paralelepípedo");
+		
+		//Instancia de ontrolador de acciones.
 		Controlador controlador = new Controlador(jugadorActual);
-		boolean enJuego = true;
-		String input = " ";
+		
+		boolean enJuego = true; //Para seguir o salir del juego.
+		
+		String input = " ";//Variable de la entrada del jugador.
+		
+		//*Insert Tutorial*
 		System.out.println("Bienvenid@!, el juego empieza aquí \n");
 		System.out.println("Puedes escribir \"/explorar\" para hacer tu primera exploración \n o puedes escribir \"/comandos\" para guiarte");
 		
+		//Bucle Principal
 		while(enJuego) {
-			input = sc.nextLine();
-			controlador.limpiarConsola();
-			enJuego = controlador.procesarInput(input);
+			input = sc.nextLine(); //Ingresa Imput
+			controlador.limpiarConsola(); //Redirecciona a la acción.
+			enJuego = controlador.procesarInput(input); //Verifica si sigue en juego o no.
 		}
 			
-		sc.close();
+		sc.close(); //Cierra Scanner 
 		System.out.println("Se ha cerrado el juego");
+		//Fin del Juego.
 	}
 
 }
