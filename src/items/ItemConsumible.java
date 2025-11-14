1e5a90db2b80d4526a09e1fc65529f7697a80a26
David <leinerdavidgp@gmail.com>
2025-11-14 20:15:11 -0300

    Mini cambios de balance

--- src/items/ItemConsumible.java
+++ src/items/ItemConsumible.java
@@ -5,14 +5,14 @@ package items;
  */
 public enum ItemConsumible {
 	POCIONVIDA("Pocion Vida", "Pocion de vida pequeña. Restaura un poco de salud", 1000, 20, 0, 0, true),
-	PANACEA("La Panacea", "La cura a todos los males. Restaura completamente la vida del jugador.",10000, 100000, 0.5, 1, true),
-	MANZANADORADA("Manzana Dorada", "AKA: Golden Apple. Cura vida y aumenta la suerte.", 15000, 50, 0.2, 1, true),
+	PANACEA("La Panacea", "La cura a todos los males. Restaura completamente la vida del jugador.",10000, 100000, 0.5, 0.05, true),
+	MANZANADORADA("Manzana Dorada", "AKA, Golden Apple. Cura vida y aumenta la suerte.", 15000, 50, 0.05, 0.1, true),
 	ZANAHORIADORADA("Zanahoria Dorada", "La comida que mas saturacion da en un juego en donde no existe la saturacion.", 4000, 40, 0, 0, true),
-	CARNEHUMANA("Carne Humana", "¿¿¿¿????", 1000, 10, 0, 0, false),
+	CARNEHUMANA("Carne Humana", "¿¿¿¿????", 1000, 10, -0.2, 0, false),
 	MANZANA("Manzana", "Puede contener gusanos.", 300, 5, 0, 0, true),
 	PLASMIO("Plasmio", "LA MEDICACION DEL FUTURO", 2000, 100, 0, 0, true),
 	POCIONDESUERTE("Pocion de suerte", "Aumenta la suerte", 50000, 0, 0.5, 0, true),
-	POCIONDEAVARICIA("Pocion de Avaricia", "Type of shit Isaac's father was pulling up home with. Aumenta las ganancias.", 50000, 0, 0, 1.5, true);
+	POCIONDEAVARICIA("Pocion de Avaricia", "Type of shit Isaac's father was pulling up home with. Aumenta las ganancias.", 50000, 0, 0, 1.2, true);
 	
 	
 	String nombre;
--- src/items/ItemEquipable.java
+++ src/items/ItemEquipable.java
@@ -6,12 +6,12 @@ package items;
 public enum ItemEquipable {
     // Define el constructor para cada tipo de arma
     DAGACEREMONIAL("Daga ceremonial", "Daga utilizada en rituales. Aumenta el daño y la suerte", 20000, 20, 0, 0, 0.5, true),  //daño promedio y aumenta la suerte que es una buena stat
-	ESCUDODEHIELOPURO("Escudo de hielo puro", "Escudo forjado con hielo magico. Aumenta todas las estadisticas defensivas", 30000, 5, 40, 0.75, 0, true), //da un poco de todo
-	GUANTELETESDELATLAS("Guanteletes del Atlas", "Guantes tecnologicos potenciados por magia runica. Aumentan todas las estadisticas.", 40000, 20, 30, 0.15, 0.5, false),
+	ESCUDODEHIELOPURO("Escudo de hielo puro", "Escudo forjado con hielo magico. Aumenta todas las estadisticas defensivas", 75000, 5, 30, 0.5, 0, true), //da un poco de todo
+	GUANTELETESDELATLAS("Guanteletes del Atlas", "Guantes tecnologicos potenciados por magia runica. Aumentan todas las estadisticas.", 40000, 20, 20, 0.05, 0.1, false),
 	ESPADAROTA("Espada Rota", "Una espada vieja y oxidada. Apenas mejor que nada.", 5000, 10, 0, 0, 0.1, false),
 	CORAZADEHIERRO("Coraza de Hierro", "Una pesada armadura de placas. Ofrece gran protección.", 25000, 0, 50, 0.25, 0.0, true),
     ANILLODEVIDA("Anillo de Vitalidad", "Un simple anillo de rubí que aumenta la vitalidad.", 18000, 0, 25, 0.0, 0.0, true),
-    COLGANTEDEFURIA("Colgante de Furia", "Un amuleto rojo que late con poder. Aumenta mucho el daño, pero te hace frágil.", 28000, 30, -10, -0.05, 0.0, true),
+    COLGANTEDEFURIA("Colgante de Furia", "Un amuleto rojo que late con poder. Aumenta mucho el daño, pero te hace frágil.", 28000, 30, -10, -0.15, 0.0, true),
     ANILLODETAHUR("Anillo de Tahúr", "Un anillo gastado que brilla levemente. Aumenta mucho la suerte.", 35000, 0, 0, 0.0, 0.75, true),
     TREBOLSECO("Trébol de cuatro hojas seco", "Un amuleto de suerte común, pero efectivo.", 12000, 0, 0, 0.0, 0.3, true),
     BOTASDEVIAJERO("Botas de Viajero", "Botas ligeras que ofrecen un equilibrio de estadísticas.", 15000, 5, 10, 0.05, 0.1, true);
